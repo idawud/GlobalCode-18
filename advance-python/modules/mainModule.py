1b19653e70fdbbd7e7cc16b689ca68be9631cfce
ismail dawud ibrahim <ismaildawud96@gmail.com>
2018-07-10 16:02:30 +0000

    added the module folder & an example in advance-python

---
+++ advance-python/modules/mainModule.py
@@ -0,0 +1,10 @@
+#from fromModule import fromModule method
+import fromModule
+fromModule.fromModule()
+fromModule.age("Yaw", 76)
+
+# Or yet another way to do it
+from fromModule import fromModule
+from fromModule import age
+fromModule()
+age("Dawud",9888)
